4ccba3dd5c1038892e3ca93ec77ea54b2d67798a
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-03-05 16:50:20 -0800

    filtering

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,19 @@
+# module Types
+#   class QueryType < Types::BaseObject
+#     # Add root-level fields here.
+#     # They will be entry points for queries on your schema.
+#
+#     # TODO: remove me
+#     field :all_links, [LinkType], null: false
+#
+#     def all_links
+#       Link.all
+#     end
+#   end
+# end
+
 module Types
-  class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :all_links, [LinkType], null: false
-
-    def all_links
-      Link.all
-    end
+  class QueryType < BaseObject
+    field :all_links, resolver: Resolvers::LinksSearch
   end
 end
